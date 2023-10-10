df93d162181b771041d3be42b94a06f202f85654
Qi Hangkong <qihangkong@outlook.com>
2023-10-10 15:30:15 +0800

    feat: add function to extra cuda fatbin data

--- cuda_runtime_api.c
+++ cuda_runtime_api.c
@@ -8,12 +8,40 @@
 #include "cudaProfiler.h"
 
 #include "cuda_api.h"
+#include "fatbinary.h"
 
 cudaError_t g_last_cudaError = cudaSuccess;
 
 char __cudaInitModule(void **fatCubinHandle) {
 	printf("%s\n", __func__);
     return 0;
+}
+
+
+void **cudaRegisterFatBinaryInternal(void *fatCubin) {
+    __fatDeviceText *fatDeviceText = (__fatDeviceText *)fatCubin;
+    COB_header *cob = (COB_header *)fatDeviceText->pCOB;
+    printf("cob.magic: %s\n", cob->magic_string);
+    printf("cob.entries_n: %ld\n", cob->entries_num);
+    bundle_entry *next_entry = (bundle_entry *)cob->entries;
+    uint32_t next_entry_size = sizeof(bundle_entry) + next_entry->entry_id_length;
+
+    for (uint32_t i=0; i<cob->entries_num; i++) {
+        bundle_entry *entry = next_entry;
+        uint32_t entry_size = next_entry_size;
+
+        printf("### Entry Info: \n");
+        printf("entry.object_offset: %lx\n", entry->code_object_offset);
+        printf("entry.object_size: %lx\n", entry->code_object_size);
+        printf("entry.id_len: %ld\n", entry->entry_id_length);
+        printf("entry.id: %s\n", (char *)entry->entry_id);
+        printf("entry size: %x\n", entry_size);
+
+        next_entry = (bundle_entry *)((uint64_t)entry + entry_size);
+        next_entry_size = sizeof(bundle_entry) + next_entry->entry_id_length;
+    }
+
+    return NULL;
 }
 
 cudaError_t CUDARTAPI __cudaPopCallConfiguration(dim3 *gridDim, dim3 *blockDim, size_t *sharedMem, void *stream) {
@@ -28,7 +56,7 @@ __host__ cudaError_t CUDARTAPI cudaLaunchKernel(const void *hostFun, dim3 gridDi
 
 void ** CUDARTAPI __cudaRegisterFatBinary(void *fatCubin) {
 	printf("%s\n", __func__);
-    return NULL;
+    return cudaRegisterFatBinaryInternal(fatCubin);
 }
 
 void CUDARTAPI __cudaRegisterFunction(void **fatCubinHandle, const char *hostFun, char *deviceFun, const char *deviceName, int thread_limit, uint3 *tid, uint3 *bid, dim3 *bDim, dim3 *gDim) {
